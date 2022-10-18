1dc8a58786bd736ca55ab90f8e85222bea3e956e
Anantha Narayanan <kvanarayanan@gmail.com>
2022-10-18 23:39:19 +0530

    Made Changes to redirect to report on success. Added changes to get info from option button

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
 import os
 import subprocess
-import shlex
+import logging
 from flask import Flask, redirect, url_for, request, render_template
-app = Flask(__name__, template_folder='template',static_folder='template')
+from flask.logging import create_logger
+
+
+#Creating Flash Object
+app = Flask(__name__, template_folder='template',static_folder='static')
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/success/<setup>')
-def success(setup):
-   return 'You have Entered %s' % setup
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      setup = request.form.get('setup', False)
-      cleanup = request.form.get('cleanup', False)
-      test = request.form.get('test', False)
-      ip = request.form['ip']
-      port = request.form['port']
+      selection = request.form.get('test')
       
-      if setup:
-          cmd = ['robot', 'test.robot']
-      if test:
-          cmd = ['robot', 'bgp_md5.robot']
       
+      #cmd = "robot --timestampoutputs --log '/app/static/mylog.html' --report '/app/static/report.html' "
+      cmd = ['robot', '--log', 'static/mylog.html', '--report', 'static/report.html']
+      
+      file = f'tests/{selection}.robot'
+      cmd.append(file)
+      LOG.info(f'cmd to execute : {cmd}')
+      LOG.info(f'cmd to execute : {os.getcwd()}')
+
       
       try:
-         subprocess.check_output(cmd)
-         #return redirect(url_for(filename='report.html'))
-         return render_template('index.html',entered_values = "Please look inro C:\\Users\\91994\\sky-auto-repo\\report.html for more details" )
+         output = subprocess.check_output(cmd)
+         LOG.info(f'Capturing robot testcase {cmd[0]} execution output : {output}')
+         return redirect(url_for('static',filename='report.html'))
+         #return render_template('index.html',entered_values = "Please look inro C:\\Users\\91994\\sky-auto-repo\\report.html for more details" )
       except Exception as e:
+         LOG.info(f'Capturing exception seen while executing robot file {cmd[0]} : {e}')
          return render_template('index.html',entered_values = f"Exception Occurred : {e}" )
 
    else:
